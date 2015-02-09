e805ac30744fe11739e9eb7a853e7e1b02c8188e
Bystroushaak <bystrousak@kitakitsune.org>
2015-02-09 21:11:01 +0100

    Small bugfix (forgotten debug code).

--- example/commands/__init__.py
+++ example/commands/__init__.py
@@ -7,10 +7,10 @@
 import os
 import sys
 
-# sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 
-from ..todobot import MAX_DATA
-from ..todobot import TIME_DIFF
-from ..todobot import HELP_FILE
-from ..todobot import MIN_TIME_DIFF
-from ..todobot import OUTPUT_TEMPLATE
+from todobot import MAX_DATA
+from todobot import TIME_DIFF
+from todobot import HELP_FILE
+from todobot import MIN_TIME_DIFF
+from todobot import OUTPUT_TEMPLATE
